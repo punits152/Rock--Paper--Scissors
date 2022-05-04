834fc0e2d5039e826562fa801c257e87a97a310f
Hariom Sharma <punits152@gmail.com>
2022-05-04 14:55:19 +0530

    Files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+// game handlers
+const getGameStatus = require("./handlers/gameStartHandler");
+
+app.route("/api/start").get(getGameStatus);
+
+module.exports = app;
